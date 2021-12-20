dfbd109687391e53723f7610198c1b59676ce7bf
Ezzat Abdallah <20049524@stu.mmu.ac.uk>
2021-12-20 23:17:30 +0000

    Changed title and fixed documentation Got rid of uint as not CLS complient

--- CustomDataStructure/Queue.cs
+++ CustomDataStructure/Queue.cs
@@ -1,39 +1,49 @@
 ﻿using System;
 
-namespace ZazCollectionGeneric
+namespace CustomDataStructures
 {
     class Queue<T> where T : IComparable
     {
         private readonly T[] arr;
         private int head, tail;
-        /// <summary>Gets the number of elements contained in the <see cref="Queue{T}"/></summary>
-        /// <returns>The number of elements contained in the <see cref="Queue{T}"/></returns>
+        /// <summary>Gets the number of elements contained in the <see cref="Queue{T}"/>.</summary>
+        /// <returns>The number of elements contained in the <see cref="Queue{T}"/>.</returns>
         public int Count { get; private set; }
-        /// <summary>Initialises a a new instance of the <see cref="Queue{T}"/> that is empty and has default initial capacity</summary>
-        public Queue(int size = 10)
+        /// <summary>Initialises a new instance of the <see cref="Queue{T}"/> that is empty and has default capacity of 10.</summary>
+        public Queue()
         {
-            arr = new T[size];
+            arr = new T[10];
             head = tail = Count = 0;
         }
-
+        /// <summary> Initialises a new instance of the <see cref="Queue{T}"/> that is empty and has variable capacity.</summary>
+        /// <param name="capacity">The number of elements the <see cref="Queue{T}"/> can contain.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Capacity is less than 0.</exception>
+        public Queue(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+            arr = new T[capacity];
+            head = tail = Count = 0;
+        }
         //
         //Queue Checks
         //
 
-        /// <summary>Gets a value indicating if the <see cref="Queue{T}"/> is empty</summary>
-        /// <returns><see langword="true"/> if <see cref="Queue{T}"/> is empty, otherwise <see langword="false"/></returns>
+        /// <summary>Gets a value indicating if the <see cref="Queue{T}"/> is empty.</summary>
+        /// <returns><see langword="true"/> if <see cref="Queue{T}"/> is empty, otherwise <see langword="false"/>.</returns>
         public bool IsEmpty()
         {
             return Count == 0;
         }
-        /// <summary>Gets a value indicating if the <see cref="Queue{T}"/> is full</summary>
-        /// <returns><see langword="true"/> if <see cref="Queue{T}"/> is full, otherwise <see langword="false"/></returns>
+        /// <summary>Gets a value indicating if the <see cref="Queue{T}"/> is full.</summary>
+        /// <returns><see langword="true"/> if <see cref="Queue{T}"/> is full, otherwise <see langword="false"/>.</returns>
         public bool IsFull()
         {
             return Count == arr.Length;
         }
-        /// <summary>Gets a value indicating if a item of type T is present in <see cref="Queue{T}"/></summary>
-        /// <returns><see langword="true"/> if item is found in the <see cref="Queue{T}"/>, otherwise <see langword="false"/></returns>
+        /// <summary>Gets a value indicating if a <see cref="T"/> item is present in <see cref="Queue{T}"/>.</summary>
+        /// <param name="item">The <see cref="T"/> item to locate in the <see cref="Queue{T}"/>. The value can be <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if item is found in the <see cref="Queue{T}"/>, otherwise <see langword="false"/>.</returns>
         public bool Contains(T item)
         {
             int i = tail;
@@ -46,56 +56,57 @@ namespace ZazCollectionGeneric
             }
             return false;
         }
-        
+
         //
         //Managing items in queue
         //
 
-        /// <summary>Adds a generic item to the end of the <see cref="Queue{T}"/></summary>
-        /// <exception cref="ArgumentOutOfRangeException"> The <see cref="Queue{T}"/> is full </exception>
+        /// <summary>Inserts <see cref="T"/> item at the tail of the <see cref="Queue{T}"/>.</summary>
+        /// <param name="item">The <see cref="T"/> item to enqueue onto the tail of the <see cref="Queue{T}"/>. The value can be <see langword="null"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException"> The Queue is full.</exception>
         public void Enqueue(T item)
         {
             if (IsFull())
-                throw new ArgumentOutOfRangeException("Cannot Enqueue on full queue");
+                throw new ArgumentOutOfRangeException(nameof(Count));
             Count++;
             arr[tail++] = item;
             tail %= arr.Length;
         }
-        /// <summary>Removes and returns the generic item at the beginning of the <see cref="Queue{T}"/></summary>
-        /// <returns>The object that is removed from the beginning of the <see cref="Queue{T}"/></returns>
-        /// <exception cref="ArgumentOutOfRangeException"> The <see cref="Queue{T}"/> is empty </exception>
+        /// <summary>Removes and returns the <see cref="T"/> item at the head of the <see cref="Queue{T}"/>.</summary>
+        /// <returns>The <see cref="T"/> item removed from the head of the <see cref="Queue{T}"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The Queue is empty.</exception>
         public T Deqeue()
         {
             if (IsEmpty())
-                throw new ArgumentOutOfRangeException("Cannot Dequeue on empty queue");
+                throw new ArgumentOutOfRangeException(nameof(Count));
             Count--;
             T temp = arr[head++];
             head %= arr.Length;
             return temp;
         }
-        /// <summary>Returns the generic item at the beginning of the <see cref="Queue{T}"/> without removing it</summary>
-        /// <returns>The generic item at the beginning of the <see cref="Queue{T}"/></returns>
-        /// <exception cref="ArgumentOutOfRangeException">The <see cref="Queue{T}"/> is empty</exception>
+        /// <summary>Returns the <see cref="T"/> item at the head of the <see cref="Queue{T}"/> without removing it.</summary>
+        /// <returns>The <see cref="T"/> item a the head of the <see cref="Queue{T}"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The Queue is empty.</exception>
         public T Peek()
         {
             if (IsEmpty())
-                throw new ArgumentOutOfRangeException("Cannot Dequeue on empty queue");
-            return arr[tail];
+                throw new ArgumentOutOfRangeException(nameof(Count));
+            return arr[head];
         }
 
         //
         //Utility
         //
 
-        /// <summary>Removes all items from the <see cref="Queue{T}"/></summary>
+        /// <summary>Removes all items from the <see cref="Queue{T}"/> and sets all values to <see langword="default"/>.</summary>
         public void Clear()
         {
             for (int i = 0; i < arr.Length; i++)
                 arr[i] = default;
             head = tail = Count = 0;
         }
-        /// <summary>Coppies an <see cref="Queue{T}"/></summary>
-        /// <returns>A new array with all current items in <see cref="Queue{T}"/></returns>
+        /// <summary>Coppies an <see cref="Queue{T}"/> to a new array.</summary>
+        /// <returns>A new array with all current items in <see cref="Queue{T}"/>.</returns>
         public T[] ToArray()
         {
             T[] outArr = new T[Count];
@@ -108,8 +119,8 @@ namespace ZazCollectionGeneric
             }
             return outArr;
         }
-        /// <summary>Gets the current data of the <see cref="Queue{T}"/> which contains the max size, current count, position of top and the contents</summary>
-        /// <returns>The current information of the <see cref="Queue{T}"/></returns>
+        /// <summary>Gets the current data of the <see cref="Queue{T}"/> which contains the max size, current count, position of head and tail, and the contents.</summary>
+        /// <returns>The current information of the <see cref="Queue{T}"/>.</returns>
         public string Info()
         {
             string info = "Max Size: " + arr.Length
--- CustomDataStructure/Stack.cs
+++ CustomDataStructure/Stack.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 
-namespace ZazCollectionGeneric
+namespace CustomDataStructures
 {
     class Stack<T> where T : IComparable
     {
         private readonly T[] arr;
         private int top;
-        /// <summary>Initialises a new instance of the <see cref="Stack{T}"/> that is empty and has default initial capacity</summary>
-        public Stack(int size = 10)
+        /// <summary>Gets the number of elements in the <see cref="Stack{T}"/>.</summary>
+        /// <returns>The number of elements in the <see cref="Stack{T}"/>.</returns>
+        public int Count { get { return top; }}
+        /// <summary>Initialises a new instance of the <see cref="Stack{T}"/> that is empty and has default capacity of 10.</summary>
+        public Stack()
         {
-            arr = new T[size];
+            arr = new T[10];
+            top = 0;
+        }
+        /// <summary> Initialises a new instance of the <see cref="Stack{T}"/> that is empty and has variable capacity.</summary>
+        /// <param name="capacity">The number of elements the <see cref="Stack{T}"/> can contain.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Capacity is less than 0.</exception>
+        public Stack(int capacity)
+        {
+            if (capacity < 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+            arr = new T[capacity];
             top = 0;
         }
 
@@ -17,26 +30,28 @@ namespace ZazCollectionGeneric
         //Stack Checks
         //
 
-        /// <summary>Gets a value indicating if the <see cref="Stack{T}"/> is empty</summary>
-        /// <returns><see langword="true"/> if <see cref="Stack{T}"/> is empty, otherwise <see langword="false"/></returns>
+        /// <summary>Gets a value indicating if the <see cref="Stack{T}"/> is empty.</summary>
+        /// <returns><see langword="true"/> if <see cref="Stack{T}"/> is empty, otherwise <see langword="false"/>.</returns>
         public bool IsEmpty()
         {
             return top == 0;
         }
-        /// <summary>Gets a value indicating if the <see cref="Stack{T}"/> is full</summary>
-        /// <returns><see langword="true"/> if <see cref="Stack{T}"/> is full, otherwise <see langword="false"/></returns>
+        /// <summary>Gets a value indicating if the <see cref="Stack{T}"/> is full.</summary>
+        /// <returns><see langword="true"/> if <see cref="Stack{T}"/> is full, otherwise <see langword="false"/>.</returns>
         public bool IsFull()
         {
             return top == arr.Length;
         }
-        /// <summary>Gets a value indicating if a item of type T is present in <see cref="Stack{T}"/></summary>
-        /// <returns><see langword="true"/> if item is found in the <see cref="Stack{T}"/>, otherwise <see langword="false"/></returns>
-        /// <exception cref="ArgumentOutOfRangeException"> The startIndex is out of bounds of the stack</exception>
-        public bool Contains(T item, uint startIndex = 0)
+        /// <summary>Gets a value indicating if a <see cref="T"/> item is present in <see cref="Stack{T}"/>.</summary>
+        /// <param name="item">The <see cref="T"/> item to locate in the <see cref="Stack{T}"/>. The value can be <see langword="null"/>.</param>
+        /// <param name="startIndex">The index to start locating the <see cref="T"/> item.</param>
+        /// <returns><see langword="true"/> if item is found in the <see cref="Stack{T}"/>, otherwise <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"> The startIndex is out of bounds of the stack.</exception>
+        public bool Contains(T item, int startIndex = 0)
         {
-            if (startIndex >= top)
-                throw new ArgumentOutOfRangeException("startIndex out of bound of stack");
-            for (uint i = startIndex; i < top; i++)
+            if (startIndex < 0 || startIndex >= top)
+                throw new ArgumentOutOfRangeException(nameof(startIndex));
+            for (int i = startIndex; i < top; i++)
                 if (arr[i].Equals(item))
                     return true;
             return false;
@@ -46,31 +61,32 @@ namespace ZazCollectionGeneric
         //Managing items in array
         //
 
-        /// <summary>Inserts a generic item at the top of the <see cref="Stack{T}"/></summary>
-        /// <exception cref="ArgumentOutOfRangeException"> The Stack is full </exception>
+        /// <summary>Inserts <see cref="T"/> item at the top of the <see cref="Stack{T}"/>.</summary>
+        /// <param name="item">The <see cref="T"/> item to push onto the top of the <see cref="Stack{T}"/>. The value can be <see langword="null"/>.</param>
+        /// <exception cref="ArgumentOutOfRangeException"> The Stack is full.</exception>
         public void Push(T item)
         {
             if (IsFull())
-                throw new ArgumentOutOfRangeException("Cannot Push on full stack");
+                throw new ArgumentOutOfRangeException(nameof(top));
             arr[top] = item;
             top++;
         }
-        /// <summary>Removes and returns the generic item at the top of the <see cref="Stack{T}"/></summary>
-        /// <returns>The generic item removed from the top of the <see cref="Stack{T}"/></returns>
-        /// <exception cref="ArgumentOutOfRangeException"> The Stack is empty </exception>
+        /// <summary>Removes and returns the <see cref="T"/> item at the top of the <see cref="Stack{T}"/>.</summary>
+        /// <returns>The <see cref="T"/> item removed from the top of the <see cref="Stack{T}"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The Stack is empty.</exception>
         public T Pop()
         {
             if (IsEmpty())
-                throw new ArgumentOutOfRangeException("Cannot Pop on empty stack");
+                throw new ArgumentOutOfRangeException(nameof(top));
             return arr[--top];
         }
-        /// <summary>Returns a generic item at the top of the <see cref="Stack{T}"/></summary>
-        /// <returns>The generic item a the top of the <see cref="Stack{T}"/></returns>
-        /// <exception cref="ArgumentOutOfRangeException"> The Stack is empty </exception>
+        /// <summary>Returns the <see cref="T"/> item at the top of the <see cref="Stack{T}"/> without removing it.</summary>
+        /// <returns>The <see cref="T"/> item a the top of the <see cref="Stack{T}"/>.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">The Stack is empty.</exception>
         public T Peek()
         {
             if (IsEmpty())
-                throw new ArgumentOutOfRangeException("Cannot Peek on empty stack");
+                throw new ArgumentOutOfRangeException(nameof(top));
             return arr[top - 1];
         }
 
@@ -78,15 +94,15 @@ namespace ZazCollectionGeneric
         //Utility
         //
 
-        /// <summary>Removes all items from the <see cref="Stack{T}"/></summary>
+        /// <summary>Removes all items from the <see cref="Stack{T}"/> and sets all values to <see langword="default"/>.</summary>
         public void Clear()
         {
             for (int i = 0; i < arr.Length; i++)
                 arr[i] = default;
             top = 0;
         }
-        /// <summary>Coppies an <see cref="Stack{T}"/></summary>
-        /// <returns>A new array with all current items in <see cref="Stack{T}"/></returns>
+        /// <summary>Coppies the <see cref="Stack{T}"/> to a new array.</summary>
+        /// <returns>A new array with all current items in <see cref="Stack{T}"/>.</returns>
         public T[] ToArray()
         {
             T[] outArr = new T[top];
@@ -94,21 +110,18 @@ namespace ZazCollectionGeneric
                 outArr[i] = arr[i];
             return outArr;
         }
-        /// <summary>Gets the number of elements in the <see cref="Stack{T}"/></summary>
-        /// <returns>The number of elements in the <see cref="Stack{T}"/></returns>
-        public int Count()
-        {
-            return top;
-        }
-        /// <summary>Gets the current data of the <see cref="Stack{T}"/> which contains the max size, position of top and the contents</summary>
-        /// <returns>The current information of the <see cref="Stack{T}"/></returns>
+        /// <summary>Gets the current data of the <see cref="Stack{T}"/> which contains the max size, count, position of top and the contents.</summary>
+        /// <returns>The current information of the <see cref="Stack{T}"/>.</returns>
         public string Info()
         {
-            string info = "Max Size: " + arr.Length + "\nTop: " + top + "\nContents:\n";
+            string info = "Max Size: " + arr.Length 
+                        + "\nCount: " + Count
+                        + "\nTop: " + top 
+                        + "\nContents:\n";
             for (int i = 0; i < arr.Length; i++)
             {
                 info += i + ": " + arr[i].ToString();
-                if (i == top - 1)
+                if (i == top )
                     info += " <--Top-->\n";
                 else
                     info += "\n";
